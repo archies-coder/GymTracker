73899f71d499c41cd9c153c8c068e4b81a96f6e0
Archis Bhoir <abhoir@egnyte.com>
2025-01-05 13:13:34 +0530

    Fixed Build

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    overrides: [
+        {
+            files: "package*.json",
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
